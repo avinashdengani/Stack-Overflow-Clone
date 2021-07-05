a63c6847b146dba9f1eb6280b926f080b3194ab0
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-05 19:36:25 +0530

    Added class justify-content-center to gring pagination links in center

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -1,6 +1,6 @@
 @if ($paginator->hasPages())
     <nav>
-        <ul class="pagination Justify-content-center">
+        <ul class="pagination justify-content-center">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
                 <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
