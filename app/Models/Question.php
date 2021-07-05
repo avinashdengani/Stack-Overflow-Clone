710949da7d21f2874f99b9e5da455c2e0f494260
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-05 18:48:49 +0530

    Added guarded, mutators, and relationship methods in Model Question

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Question extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    /**
+     * MUTATORS
+    */
+
+    public function setTitleAttribute(string $title)
+    {
+        $this->attributes['title'] = $title;
+        $this->attributes['slug'] = Str::slug($title);
+    }
+
+    /**
+     * RELATIONSHIP METHODS
+     */
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
