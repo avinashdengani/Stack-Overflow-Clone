4c099569f2fa81a443859266b57f32c739d5138b
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-10 21:05:28 +0530

    Displayed answers, usernames, time,etc in show.blade.php

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -9,8 +9,60 @@
                     <div class="card-body">
                         {!! $question->body !!}
                     </div>
+                    <div class="card-footer">
+                        <div class="d-flex justify-content-between mr-3">
+                            <div></div>
+                            <div class="d-flex flex-column">
+                                <div class="text-muted mb-3 text-right">
+                                    <p>Asked {{$question->created_date}}</p>
+                                </div>
+                                <div class="d-flex mb-2">
+                                    <div>
+                                        <img src="{{$question->owner->avatar}}" alt="user-avatar">
+                                    </div>
+                                    <div class=" ml-2">
+                                        <p>{{$question->owner->name}}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
+
+        <div class="row justify-content-center mt-4">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="mt-0" >{{Str::plural('Answer', $question->answers_count)}}</h3>
+                    </div>
+                    <div class="card-body">
+                        @foreach ($question->answers as $answer)
+                            {!! $answer->body !!}
+                            <div class="d-flex justify-content-between mr-3">
+                                <div></div>
+
+                            <div class="d-flex flex-column">
+                                <div class="text-muted mb-3 text-right">
+                                    <p>Answered {{$answer->created_date}}</p>
+                                </div>
+                                <div class="d-flex mb-2">
+                                    <div>
+                                        <img src="{{$answer->author->avatar}}" alt="user-avatar">
+                                    </div>
+                                    <div class=" ml-2">
+                                        <p>{{$answer->author->name}}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            <hr>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 @endsection
