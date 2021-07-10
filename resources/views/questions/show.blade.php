47ad55459f83e479bedc4c00002605fd11a243bd
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-10 19:23:05 +0530

    Created show.blade.php to display Question

---
+++ resources/views/questions/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header"><h1>{{ $question->title }}</h1></div>
+                    <div class="card-body">
+                        {!! $question->body !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
