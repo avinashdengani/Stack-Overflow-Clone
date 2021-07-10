0b8e122a8c75e0ea89cf33a014d1074e9f3f9d9c
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-10 20:19:04 +0530

    Added a static method boot to increase answers_count as someone answered

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -10,6 +10,14 @@ class Answer extends Model
     use HasFactory;
     protected $guarded = ['id'];
 
+    public static function boot()
+    {
+        parent::boot();
+        static::created(function(Answer $answer){
+            $answer->question->increment('answers_count');
+        });
+    }
+
     public function question()
     {
         return $this->belongsTo(Question::class);
