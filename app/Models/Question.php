7a168d264cbf827b60648e28f8574d1e698c322d
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-10 19:01:35 +0530

    Changed url form id to slug

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -15,7 +15,7 @@ class Question extends Model
 
     public function getUrlAttribute()
     {
-        return "questions/{$this->id}";
+        return "questions/{$this->slug}";
     }
 
     public function getCreatedDateAttribute()
