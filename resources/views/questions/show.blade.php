4bf266f03f6c7fc0a69f3616b9be83d347a93275
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-11 17:34:12 +0530

    Displayed answers of particular question and added vote up down buttons, etc

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -11,7 +11,17 @@
                     </div>
                     <div class="card-footer">
                         <div class="d-flex justify-content-between mr-3">
-                            <div></div>
+                            <div class="d-flex">
+                                <div>
+                                    <a href="" title="Up Vote" class="vote-up d-block text-center text-black-50"><i class="fa fa-caret-up fa-3x" ></i></a>
+                                    <h4 class="votes-count text-muted text-center m-0">45</h4>
+                                    <a href="" title="Down Vote" class="vote-down d-block text-center text-black-50"><i class="fa fa-caret-down fa-3x" ></i></a>
+                                </div>
+                                <div class="ml-5 mt-3">
+                                    <a href=""  title="Mark as favorite" class="favorite d-block text-center text-dark mb-2"><i class="fa fa-star fa-2x" ></i></a>
+                                    <h4 class="views-count text-muted text-center m-0">123</h4>
+                                </div>
+                            </div>
                             <div class="d-flex flex-column">
                                 <div class="text-muted mb-3 text-right">
                                     <p>Asked {{$question->created_date}}</p>
@@ -20,7 +30,7 @@
                                     <div>
                                         <img src="{{$question->owner->avatar}}" alt="user-avatar">
                                     </div>
-                                    <div class=" ml-2">
+                                    <div class="ml-2 mt-1">
                                         <p>{{$question->owner->name}}</p>
                                     </div>
                                 </div>
@@ -31,38 +41,7 @@
             </div>
         </div>
 
-        <div class="row justify-content-center mt-4">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="mt-0" >{{Str::plural('Answer', $question->answers_count)}}</h3>
-                    </div>
-                    <div class="card-body">
-                        @foreach ($question->answers as $answer)
-                            {!! $answer->body !!}
-                            <div class="d-flex justify-content-between mr-3">
-                                <div></div>
-
-                            <div class="d-flex flex-column">
-                                <div class="text-muted mb-3 text-right">
-                                    <p>Answered {{$answer->created_date}}</p>
-                                </div>
-                                <div class="d-flex mb-2">
-                                    <div>
-                                        <img src="{{$answer->author->avatar}}" alt="user-avatar">
-                                    </div>
-                                    <div class=" ml-2">
-                                        <p>{{$answer->author->name}}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                            <hr>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-
-        </div>
+    @include('answers._index')
+    @include('answers._create')
     </div>
 @endsection
