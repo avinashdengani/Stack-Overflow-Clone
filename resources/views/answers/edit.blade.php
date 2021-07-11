ba5b591fbd927f6853c90d496f7b116d728ca127
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-11 17:32:36 +0530

    Created a page to edit answer

---
+++ resources/views/answers/edit.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center mt-4">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Edit your Answer</h3>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{route('questions.answers.update', [$question->id, $answer->id])}}" method="POST" id="create-answer-form">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group">
+                                <input type="hidden" id="body" name="body" value="{{ old('body', $answer->body) }}">
+                                <trix-editor input="body" class="form-control  @error('body') is-invalid @enderror" ></trix-editor>
+                                @error('body')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-outline-success" >Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.min.js" integrity="sha512-2RLMQRNr+D47nbLnsbEqtEmgKy67OSCpWJjJM394czt99xj3jJJJBQ43K7lJpfYAYtvekeyzqfZTx2mqoDh7vg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js" integrity="sha512-37T7leoNS06R80c8Ulq7cdCDU5MNQBwlYoy1TX/WUsLFC2eYNqtKlV0QjH7r8JpG/S0GUMZwebnVFLPd6SU5yg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script>
+        $("#create-answer-form").validate({
+            rules: {
+                body: {
+                    required: true
+                }
+            },
+            errorElement: 'p',
+            errorPlacement: function(error, element) {
+                if (error) {
+                    error.insertAfter(element);
+                    error.addClass('text-danger');
+                }
+            }
+        });
+
+    </script>
+@endsection
+
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.css" integrity="sha512-CWdvnJD7uGtuypLLe5rLU3eUAkbzBR3Bm1SFPEaRfvXXI2v2H5Y0057EMTzNuGGRIznt8+128QIDQ8RqmHbAdg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
