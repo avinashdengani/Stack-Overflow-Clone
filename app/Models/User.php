d989d1af9c72b31c88337ca5845c43ce686cb7c1
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-12 20:14:28 +0530

    Added methods to check for votes

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,33 @@ class User extends Authenticatable
         $name = $this->name;
         return "https://ui-avatars.com/api/?name={$name}&rounded=true&size={$size}";
     }
+    public function hasQuestionUpVote(Question $question)
+    {
+        return auth()->user()->votesQuestions()->where(['vote' => 1, 'vote_id' => $question->id, 'vote_type' => Question::class])->exists();
+    }
+    public function hasQuestionDownVote(Question $question)
+    {
+        return auth()->user()->votesQuestions()->where(['vote' => -1, 'vote_id' => $question->id, 'vote_type' => Question::class])->exists();
+    }
+    public function hasVoteForQuestion(Question $question)
+    {
+        return $this->hasQuestionUpVote($question) || $this->hasQuestionDownVote($question);
+    }
+
+    public function hasAnswerUpVote(Answer $answer)
+    {
+        return auth()->user()->votesAnswers()->where(['vote' => 1, 'vote_id' => $answer->id, 'vote_type' => Answer::class])->exists();
+    }
+    public function hasAnswerDownVote(Answer $answer)
+    {
+        return auth()->user()->votesAnswers()->where(['vote' => -1, 'vote_id' => $answer->id, 'vote_type' => Answer::class])->exists();
+    }
+    public function hasVoteForAnswer(Answer $answer)
+    {
+        return $this->hasAnswerUpVote($answer) || $this->hasAnswerDownVote($answer);
+    }
+
+
     /**
      * RELATIONSHIP METHODS
      */
@@ -62,5 +89,15 @@ class User extends Authenticatable
     public function favorites()
     {
         return $this->belongsToMany(Question::class)->withTimestamps();
+    }
+
+
+    public function votesQuestions()
+    {
+        return $this->morphedByMany(Question::class, 'vote')->withTimestamps();
+    }
+    public function votesAnswers()
+    {
+        return $this->morphedByMany(Answer::class, 'vote')->withTimestamps();
     }
 }
