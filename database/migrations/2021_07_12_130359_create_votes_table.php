d69b8bb128f3b282118a3df8bbe0214412293c33
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-12 20:14:41 +0530

    Added columns to votes table

--- database/migrations/2021_07_12_130359_create_votes_table.php
+++ database/migrations/2021_07_12_130359_create_votes_table.php
@@ -15,10 +15,19 @@ class CreateVotesTable extends Migration
     {
         Schema::create('votes', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('vote_type');
+            $table->unsignedBigInteger('vote_id');
+            $table->tinyInteger('vote');
             $table->timestamps();
+
+            $table->unique(['user_id', 'vote_id', 'vote_type']);
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
     }
-
     /**
      * Reverse the migrations.
      *
