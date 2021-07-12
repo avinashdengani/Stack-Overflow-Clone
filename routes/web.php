a6e2d763a8ed1b61f827d68f51ca7eecfc4bd3df
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-12 20:09:53 +0530

    Added routes for maintaing votes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AnswersController;
 use App\Http\Controllers\FavoritesController;
 use App\Http\Controllers\QuestionsController;
+use App\Http\Controllers\VotesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +30,5 @@ Route::resource('questions.answers', AnswersController::class)->except(['index',
 Route::put('answers/{answer}/best-answer', [AnswersController::class, 'bestAnswer'])->name('answers.bestAnswer');
 Route::post('questions/{question}/favorite',[FavoritesController::class, 'store'] )->name('questions.favorite');
 Route::delete('questions/{question}/unfavorite',[FavoritesController::class, 'destroy'] )->name('questions.unfavorite');
+Route::post('questions/{question}/vote/{vote}',[VotesController::class, 'voteQuestion'])->name('questions.vote');
+Route::post('answers/{answer}/vote/{vote}',[VotesController::class, 'voteAnswer'])->name('answers.vote');
