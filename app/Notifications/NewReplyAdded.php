242ce62ba4a158f327c72e07d3b620885d2d1862
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-13 11:18:48 +0530

    Added database table functionality

--- app/Notifications/NewReplyAdded.php
+++ app/Notifications/NewReplyAdded.php
@@ -30,7 +30,7 @@ class NewReplyAdded extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['mail', 'database'];
     }
 
     /**
@@ -56,7 +56,7 @@ class NewReplyAdded extends Notification
     public function toArray($notifiable)
     {
         return [
-            //
+            'question' => $this->question
         ];
     }
 }
