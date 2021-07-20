b57e6cd8fbf8e384acdf20dcda1d2ec46d3a2926
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-20 13:57:53 +0530

    bug fixes in FavoritesController

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -4,17 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FavoritesController extends Controller
 {
     public function favorite(Question $question)
     {
-        $question->views()->update(['is_favorite' => 1]);
+        $question->favoritesUserId()->update(['is_favorite' => 1]);
         return redirect()->back();
     }
     public function unfavorite(Question $question)
     {
-        $question->FavoritesUserId()->update(['is_favorite' => 0]);
+        $question->favoritesUserId()->update(['is_favorite' => 0]);
         return redirect()->back();
     }
 }
