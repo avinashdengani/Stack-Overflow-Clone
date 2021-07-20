d93735c9b53d7c4af09e436a12b9d2806acede4d
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-20 13:56:20 +0530

    added functionality to Dashboard controller to return index view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        if(auth()->check()) {
+            $your_questions = auth()->user()->questions()->with('owner')->get();
+            return view('index', compact(['your_questions']));
+        }
+        return view('index');
+    }
 }
