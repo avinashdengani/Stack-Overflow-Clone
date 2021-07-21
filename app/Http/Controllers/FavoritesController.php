d02e7cf0a147b62ecf3d32f95a8fc2254e3dd439
Avinash Dengani <avinashdengani123@gmail.com>
2021-07-21 18:38:24 +0530

    Added functionality in FavoritesController to  get a notification

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use App\Notifications\NewFavoriteMarked;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -16,6 +17,8 @@ class FavoritesController extends Controller
     public function favorite(Question $question)
     {
         $question->favoritesUserId()->update(['is_favorite' => 1]);
+        $question->owner->notify(new NewFavoriteMarked($question));
+        session()->flash('success', 'Yor answer has been submitted successfully!');
         return redirect()->back();
     }
     public function unfavorite(Question $question)
